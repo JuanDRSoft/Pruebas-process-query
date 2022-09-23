01ac890324929b7bc47c0b18b01923ec15cea9a4
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-09-23 14:48:34 -0500

    Add files via upload

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const { template } = require('../html/template');
+const { templatePassword } = require('../html/password/template');
 
 const sendEmail = async (emailTo, requestAccion, requestProceso) => {
   const { llaveProceso, fechaProceso } = requestProceso;
@@ -27,6 +28,34 @@ const sendEmail = async (emailTo, requestAccion, requestProceso) => {
   });
 };
 
+const forgotPasswordEmail = async (emailTo, lawyer) => {
+  const { name } = lawyer;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'solutioprocess',
+      pass: 'gmepudoqeqhqcawl'
+    },
+    tls: { rejectUnauthorized: false }
+  });
+
+  const mailOptions = {
+    from: 'solutioprocess@gmail.com',
+    to: emailTo,
+    subject: `Reestablecimiento de contraseña: ${name}`,
+    html: templatePassword(lawyer)
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+};
+
 module.exports = {
-  sendEmail
+  sendEmail,
+  forgotPasswordEmail
 };
