1b387bed64a2b629fb71e79b064b44105291a22a
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-09-23 14:50:47 -0500

    Add files via upload

--- src/routes/Lawyer.js
+++ src/routes/Lawyer.js
@@ -1,17 +1,21 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-const LawyerController = require("../controllers/LawyerController");
-const auth = require("../middleware/auth");
+const LawyerController = require('../controllers/LawyerController');
+const auth = require('../middleware/auth');
 
-router.route("/").get(LawyerController.index).post(LawyerController.create);
+router.route('/').get(LawyerController.index).post(LawyerController.create);
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(auth, LawyerController.find, LawyerController.show)
   .put(LawyerController.find, LawyerController.update)
   .delete(LawyerController.find, LawyerController.destroy);
 
-router.route("/find/email").post(LawyerController.findByEmail);
+router.route('/find/email').post(LawyerController.findByEmail);
+
+router.route('/password').post(LawyerController.forgotPassword);
+
+router.route('/newPassword').post(LawyerController.newPassword);
 
 module.exports = router;
