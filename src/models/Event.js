12237ed962613efa2c90f3a21d48fe6935e47936
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-10-19 19:38:06 -0500

    Add files via upload

---
+++ src/models/Event.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+let eventSchema = new mongoose.Schema({
+  creado: { type: Date, default: Date.now() },
+  notification: { type: Boolean, default: false },
+  start: Date,
+  end: Date,
+  title: String,
+  process: String,
+  lawyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Lawyer'
+  }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
