f832486c40ca703b3453d8a2556f3d878ed824a2
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-10-19 19:38:34 -0500

    Add files via upload

---
+++ src/controllers/EventController.js
@@ -0,0 +1,51 @@
+const Event = require('../models/Event');
+
+function findByLawyerAll(req, res, next) {
+  Event.find({ lawyer: req.usuario })
+    .sort([['startDate', +1]])
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      res.json('err');
+    });
+}
+
+async function create(req, res) {
+  let params = req.body;
+
+  Event.create(params)
+    .then((event) => {
+      res.json(event);
+      req.event = event;
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(422).json({ error });
+    });
+}
+
+function destroy(req, res) {
+  req.event
+    .remove()
+    .then((doc) => {
+      res.json({});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+}
+
+function find(req, res, next) {
+  Event.findById(req.params.id)
+    .then((event) => {
+      req.event = event;
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = { findByLawyerAll, create, destroy, find };
