e42ff6c4e56b6ae5f1435b5d6919141ec280e9a9
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-10-28 12:10:26 -0500

    Add files via upload

---
+++ src/utils/eventNotification.js
@@ -0,0 +1,38 @@
+const { getDate, getMonth, getYear } = require('date-fns');
+const Event = require('../models/Event');
+
+const requestEvents = async () => {
+  const eventsData = await Event.find({});
+
+  for (let i = 0; i < eventsData.length; i++) {
+    const { title, end, _id } = eventsData[i];
+
+    console.log(i, title, end);
+
+    const resta = end - Date.now();
+    const doc = await Event.findById(_id);
+
+    console.log(getDate(resta));
+    if (getDate(resta) === 1) {
+      try {
+        doc.notification = true;
+        await doc.save();
+
+        console.log(i, 'Notificacion Actualizada');
+      } catch (error) {
+        console.log(error);
+      }
+    } else if (end < Date.now()) {
+      try {
+        doc.notification = false;
+        await doc.save();
+
+        console.log(i, 'Notificacion Desactivada');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
+
+module.exports = { requestEvents };
